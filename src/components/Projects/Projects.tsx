5c3ec119927f110df96c7199d9011d237257178f
tikaya <tatiana.chefox@gmail.com>
2023-09-28 19:28:05 +0300

    adding a conditional list of projects

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -2,6 +2,13 @@ export const Projects = () => {
     return (
         <div>
             <h1>Projects</h1>
+            <div>
+                <ul>
+                    <li>Project 1</li>
+                    <li>Project 2</li>
+                    <li>Project 3</li>
+                </ul>
+            </div>
         </div>
     )
 }
