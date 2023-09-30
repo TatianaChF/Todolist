666c3dad2386c2a565ca8f199c9637f0475974da
tikaya <tatiana.chefox@gmail.com>
2023-09-30 09:48:59 +0300

    displaying the project name

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -1,16 +1,38 @@
-import { useState } from "react"
+import { useEffect, useState } from "react"
+
+type Data = {
+    id: string,
+    title: string,
+    status: string
+}
 
 export const Projects = () => {
-    const [projects, setProjects] = useState([]);
+    const [data, setData] = useState<Data[]>();
+
+    const fetchProjects = () => {
+        fetch("./projects.json")
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            setData(data);
+        }).catch((e: Error) => {
+            console.log(e.message)
+        })
+    }
+
+    useEffect(() => {
+        fetchProjects();
+    }, [])
+
     return (
         <div>
             <h1>Projects</h1>
             <div>
-                <ul>
-                    <li>Project 1</li>
-                    <li>Project 2</li>
-                    <li>Project 3</li>
-                </ul>
+                {
+                    data?.map((value) => {
+                        return <p key={value.id}>{value.title}</p>
+                    })
+                }
             </div>
         </div>
     )
