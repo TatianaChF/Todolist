7840a8b9e9042a3730ec4f15e4514463e0c323bc
tikaya <tatiana.chefox@gmail.com>
2023-10-02 09:23:56 +0300

    export task componet

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,7 +1,12 @@
+import { Task } from "./Task/Task"
+
 export const Tasks = () => {
     return (
         <div>
             <h1>Tasks</h1>
+            <div>
+                <Task />
+            </div>
         </div>
     )
 }
