a3adce560cf32bfabac0b00854659177db7ce3a1
tikaya <tatiana.chefox@gmail.com>
2023-10-02 09:10:06 +0300

    adding provider and RouterProvider

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Providers } from './utils/Providers';
+import { RouterProvider } from 'react-router';
+import { router } from './utils/Router';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Providers>
+      <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
+    </Providers>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
