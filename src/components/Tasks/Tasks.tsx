7bb56c26e9431fa659b4d2303a036c34f7341bc0
tikaya <tatiana.chefox@gmail.com>
2023-10-03 11:17:28 +0300

    adding tasks component

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -1,11 +1,47 @@
+import { useEffect, useState } from "react";
 import { Task } from "./Task/Task"
+import { useNavigate, useParams } from "react-router";
+
+type TasksType = {
+    id: number,
+    id_projects: number,
+    title: string,
+    status: string
+}
 
 export const Tasks = () => {
+    const [tasks, setTasks] = useState<TasksType[]>();
+    const { id } = useParams();
+    const navigate = useNavigate(); 
+
+    const fetchTasks = () => {
+        fetch("tasks.json").then(response => {
+            console.log(response);
+            return response.json()
+        }).then(data => {
+            console.log(data);
+            setTasks(data);
+            console.log(tasks)
+        }).catch((e: Error) => {
+            console.log(e.message);
+        })
+    }
+
+    useEffect(() => {
+        fetchTasks();
+    }, [])
+
     return (
         <div>
             <h1>Tasks</h1>
             <div>
-                <Task />
+                {
+                    tasks?.map((items) => {
+                        return(
+                            <Task key={items.id} title={items.title} status={items.status} />
+                        )
+                    })
+                }
             </div>
         </div>
     )
