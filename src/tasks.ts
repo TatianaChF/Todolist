0ca87b90db987d48b18ecbfe265d273c76cd3de1
tikaya <tatiana.chefox@gmail.com>
2023-10-04 18:56:04 +0300

    creating a new file to store tasks

---
+++ src/tasks.ts
@@ -0,0 +1,36 @@
+import { COLUMN_NAMES } from "./constans";
+
+const { QUEUE } = COLUMN_NAMES;
+
+export const tasks = [
+    {
+        id: 0,
+        id_projects: 0,
+        title: "Task 380",
+        column: QUEUE
+    },
+    {
+        id: 1,
+        id_projects: 0,
+        title: "Task 381",
+        column: QUEUE
+    },
+    {
+        id: 2,
+        id_projects: 0,
+        title: "Task 382",
+        column: QUEUE
+    },
+    {
+        id: 3,
+        id_projects: 1,
+        title: "Task 100",
+        column: QUEUE
+    },
+    {
+        id: 4,
+        id_projects: 1,
+        title: "Task 101",
+        column: QUEUE
+    }
+]
