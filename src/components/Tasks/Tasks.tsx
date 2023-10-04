a421f86d2c4627bfee22676c2dbeb82b5755f0aa
tikaya <tatiana.chefox@gmail.com>
2023-10-04 19:07:59 +0300

    creating a new template for a component with tasks

--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -2,6 +2,10 @@ import { useEffect, useState } from "react"
 import { Task } from "./Task/Task"
 import { useNavigate, useParams } from "react-router"
 import styles from "./Tasks.module.css"
+import { tasks } from "../../tasks"
+import { COLUMN_NAMES } from "../../constans"
+import { DndProvider } from "react-dnd"
+import { HTML5Backend } from "react-dnd-html5-backend"
 
 type TasksType = {
     id: number,
@@ -11,69 +15,14 @@ type TasksType = {
 }
 
 export const Tasks = () => {
-    const [tasks, setTasks] = useState<TasksType[]>();
-    const [currentBoard, setCurrentBoard] = useState(null);
-    const [currentItem, setCurrentItem] = useState(null);
-    const { id } = useParams();
-
-    const fetchTasks = () => {
-        fetch("tasks.json").then(response => {
-            return response.json()
-        }).then(data => {
-            setTasks(data);
-        }).catch((e: Error) => {
-            console.log(e.message);
-        })
-    }
-
-    useEffect(() => {
-        fetchTasks();
-    }, [])
+    const [items, setItems] = useState(tasks);
+    const { QUEUE, DEV, DONE } = COLUMN_NAMES;
 
     return (
-        <div>
-            <h1 className={styles.header}>Tasks</h1>
-            <div className={styles.container}>
-                <div className={styles.container_column}>
-                    <h4>Queue</h4>
-                    {
-                        tasks?.filter((items) => items.id_projects.toString() === id).filter((item) => item.status === "queue")
-                        .map((items) => {
-                            return(
-                                <Task key={items.id} 
-                                    title={items.title} 
-                                    status={items.status} />
-                            )
-                        })
-                    }
-                </div>
-                <div className={styles.container_column}>
-                    <h4>Development</h4>
-                    {
-                        tasks?.filter((items) => items.id_projects.toString() === id).filter((item) => item.status === "dev")
-                        .map((items) => {
-                            return(
-                                <Task key={items.id} 
-                                    title={items.title} 
-                                    status={items.status} />
-                            )
-                        })
-                    }
-                </div>
-                <div className={styles.container_column}>
-                    <h4>Done</h4>
-                    {
-                        tasks?.filter((items) => items.id_projects.toString() === id).filter((item) => item.status === "done")
-                        .map((items) => {
-                            return(
-                                <Task key={items.id} 
-                                    title={items.title} 
-                                    status={items.status} />
-                            )
-                        })
-                    }
-                </div>
-            </div>
+        <div className={styles.container}>
+            <DndProvider backend={HTML5Backend}>
+                
+            </DndProvider>
         </div>
     )
 }
