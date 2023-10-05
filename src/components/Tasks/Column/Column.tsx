07150438e4dc58c4f70d0ef2709726d7bf697a00
tikaya <tatiana.chefox@gmail.com>
2023-10-05 09:01:06 +0300

    adding props types

--- src/components/Tasks/Column/Column.tsx
+++ src/components/Tasks/Column/Column.tsx
@@ -1,4 +1,10 @@
-export const Column = () => {
+import { COLUMN_NAMES } from "../../../constans"
+
+type PropsColumn = {
+    title: string
+}
+
+export const Column = (props: PropsColumn) => {
     return (
         <div></div>
     )
