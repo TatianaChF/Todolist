51eff536546317348be95f43c06dfa8a93cada95
tikaya <tatiana.chefox@gmail.com>
2023-10-08 20:46:41 +0300

    wrap the application in DndProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
+import { DndProvider } from 'react-dnd';
 import './App.css';
 import { Outlet } from 'react-router';
+import { HTML5Backend } from 'react-dnd-html5-backend';
 
 function App() {
   return (
-    <div className="App">
-      <Outlet />
-    </div>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Outlet />
+      </div>
+    </DndProvider>
   );
 }
 
