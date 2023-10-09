bc180f0debe0161c03bd5c39e6863f46f0efe0c7
tikaya <tatiana.chefox@gmail.com>
2023-10-09 21:48:05 +0300

    adding onChange

--- src/components/CreateTask/CreateTask.tsx
+++ src/components/CreateTask/CreateTask.tsx
@@ -1,18 +1,25 @@
 import { Dispatch, SetStateAction, useState } from "react"
 import { TasksType } from "../Tasks/Tasks"
 import styles from "./CreateTask.module.css"
+import { v4 as uuidv4 } from 'uuid';
 
 export type TasksProps = {
     tasks: TasksType[],
-    setTasks: Dispatch<SetStateAction<TasksType[]>>
+    setTasks: Dispatch<SetStateAction<{ 
+        id: string; 
+        id_projects: number; 
+        title: string; 
+        column: string; 
+    }[]>>
 }
 
 export const CreateTask = ({tasks, setTasks}: TasksProps) => {
-    const [task, setTask] = useState();
+    const [task, setTask] = useState<TasksType>();
 
     return (
         <form>
-            <input className={styles.input_add} placeholder="Task name" type="text" />
+            <input className={styles.input_add} placeholder="Task name" type="text" 
+            onChange={(e) => setTask({...task, id: uuidv4(), title: e.target.value })} />
             <button className={styles.button_add}>Create</button>
         </form>
     )
