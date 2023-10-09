fff0de5b8611bb025d492c41336842eca7fcda70
tikaya <tatiana.chefox@gmail.com>
2023-10-09 18:20:33 +0300

    creating a component to add a task

---
+++ src/components/CreateTask/CreateTask.tsx
@@ -0,0 +1,7 @@
+export const CreateTask = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
--- src/components/Tasks/Tasks.tsx
+++ src/components/Tasks/Tasks.tsx
@@ -2,10 +2,10 @@ import { SetStateAction, useState } from "react"
 import styles from "./Tasks.module.css"
 import { tasks } from "../../tasks"
 import { COLUMN_NAMES } from "../../constans"
-import { DndProvider, useDrop } from "react-dnd"
-import { HTML5Backend } from "react-dnd-html5-backend"
+import { useDrop } from "react-dnd"
 import { Column } from "./Column/Column"
 import { TASK_DND_TYPE, Task } from "./Task/Task"
+import { CreateTask } from "../CreateTask/CreateTask"
 
 export type TasksType = {
     id: number,
@@ -18,39 +18,30 @@ export const Tasks = () => {
     const [items, setItems] = useState(tasks);
     const { QUEUE, DEV, DONE } = COLUMN_NAMES;
 
-    const [{ isOver }, drop] = useDrop(() => ({
-        accept: TASK_DND_TYPE,
-        drop: (item: any) => addItemToSection(item.name),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        })
-    }));
-
-    const addItemToSection = (name: string) => {
-        console.log("droped", name);
-    }
-
     const returnItemsForColumn = (columnName: string) => {
         return items.filter((item) => item.column === columnName).map((item, index) => (
             <Task key={item.id} 
                 name={item.title} 
-                currentColumnName={item.column} 
+                column={item.column} 
                 setItems={setItems} 
                 index={index} />
         ))
     }
 
     return (
-        <div ref={drop} className={styles.container}>
-                <Column title={ QUEUE }>
+        <div>
+            <CreateTask />
+            <div className={styles.container}>
+                <Column title={ QUEUE } items={items} setItems={setItems}>
                     {returnItemsForColumn(QUEUE)}
                 </Column>
-                <Column title={ DEV }>
+                <Column title={ DEV } items={items} setItems={setItems}>
                     {returnItemsForColumn(DEV)}
                 </Column>
-                <Column title={ DONE }>
+                <Column title={ DONE } items={items} setItems={setItems}>
                     {returnItemsForColumn(DONE)}
                 </Column>
+            </div>
         </div>
     )
 }
