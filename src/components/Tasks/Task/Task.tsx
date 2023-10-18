d01c780aad0dc398b5b64b3bb62ba8862d3cf734
tikaya <tatiana.chefox@gmail.com>
2023-10-18 20:24:11 +0300

    output list of tasks from json

--- src/components/Tasks/Task/Task.tsx
+++ src/components/Tasks/Task/Task.tsx
@@ -4,22 +4,15 @@ import { Dispatch, SetStateAction, useRef } from "react"
 import { COLUMN_NAMES } from "../../../constans"
 import { DragSourceMonitor } from 'react-dnd'
 import { TasksType } from "../Tasks"
-import { TasksProps } from "../../CreateTask/CreateTask"
-
-type ItemType = {
-    type: string,
-    index: number, 
-    name: string, 
-    currentColumnName: string
-}
 
 export const TASK_DND_TYPE = "TASK_DND_TYPE";
 
-export const Task = ({tasks, setTasks}: TasksProps) => {
+export const Task = ({id, id_projects, title, column}: TasksType) => {
     
     return (
         <div className={styles.task_container}>
-            
+            {title}
         </div>
     )
 }
+
