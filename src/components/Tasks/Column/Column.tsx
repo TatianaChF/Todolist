325656b78e9ba79cbb41f55a8accedd2beb9c081
tikaya <tatiana.chefox@gmail.com>
2023-10-23 18:35:33 +0300

    update

--- src/components/Tasks/Column/Column.tsx
+++ src/components/Tasks/Column/Column.tsx
@@ -1,41 +1,7 @@
-import { useDrop } from "react-dnd"
-import styles from "./Column.module.css"
-import { TASK_DND_TYPE } from "../Task/Task"
-import { COLUMN_NAMES } from "../../../constans"
-import { TasksType } from "../Tasks"
-import { Dispatch, SetStateAction } from "react"
-
-type PropsColumn = {
-    children: JSX.Element[],
-    title: string,
-    items: TasksType[],
-    setItems: Dispatch<SetStateAction<TasksType[]>>
-}
-
-export const Column = (props: PropsColumn) => {
-
-    /* const [{ isOver }, drop] = useDrop(() => ({
-        accept: TASK_DND_TYPE,
-        drop: (item: any) => addItemToSection(item.name, item.column),
-        collect: (monitor) => ({
-            isOver: !!monitor.isOver()
-        })
-    }));
-
-    const addItemToSection = (name: string, column: string) => {
-        props.setItems((prev: TasksType[]) => {
-            const mItem = prev.map(t => {
-                if(t.title=== props.items.) {
-                    return {...t, props.title: props.items.column}
-                }
-            })
-        })
-    } */
-
+export const Column = () => {
     return (
-        <div className={styles.column}>
-            <p>{props.title}</p>
-            {props.children}
+        <div>
+            
         </div>
     )
-}
+} 
