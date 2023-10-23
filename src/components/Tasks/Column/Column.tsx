c514d192f2f5c931a47c50d3a8dfcbab3cf703a1
tikaya <tatiana.chefox@gmail.com>
2023-10-23 19:42:34 +0300

    displaying tasks and adding filtering by columns

--- src/components/Tasks/Column/Column.tsx
+++ src/components/Tasks/Column/Column.tsx
@@ -1,6 +1,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { TasksType } from "../Tasks";
 import styles from "./Column.module.css";
+import { Task } from "../Task/Task";
 
 type ColumnPropsType = {
     status: string,
@@ -8,15 +9,27 @@ type ColumnPropsType = {
     setItems: Dispatch<SetStateAction<TasksType[]>>,
     queue: TasksType[],
     dev: TasksType[],
-    done: TasksType[]
+    done: TasksType[],
+    id: string | undefined
 }
 
-export const Column = ({status}: ColumnPropsType) => {
+export const Column = ({ status, items, setItems, queue, dev, done, id }: ColumnPropsType) => {
+    let count = queue.length;
+
     return (
         <div className={styles.column}>
             <h2>
                 {status}
             </h2>
+            {
+                items?.filter(tasks => tasks.id_projects?.toString() === id).filter(task => task.column === status)
+                .map((value) => {
+                    return (
+                        <Task key={value.id} id={value.id} id_projects={value.id_projects} 
+                        title={value.title} column={value.column} />
+                    )
+                })
+            }
         </div>
     )
 } 
