fff1c41fee72ca4c261ca02cb0e7a1a7a2dd66ab
tikaya <tatiana.chefox@gmail.com>
2023-10-25 19:02:36 +0300

    creating a task delete function template

--- src/components/Tasks/Task/Task.tsx
+++ src/components/Tasks/Task/Task.tsx
@@ -5,11 +5,15 @@ import remove_task from "./../../../assets/remove.svg"
 export const TASK_DND_TYPE = "TASK_DND_TYPE";
 
 export const Task = ({id, id_projects, title, column}: TasksType) => {
+
+    const removeTask = (id: string) => {
+        console.log(title);
+    }
     
     return (
         <div className={styles.task_container}>
             <p className={styles.title_task}>{title}</p>
-            <svg className={styles.img_remove} viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#3d3d3d" stroke="#3d3d3d">
+            <svg onClick={() => removeTask(id)} className={styles.img_remove} viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#3d3d3d" stroke="#3d3d3d">
                 <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
                 <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
