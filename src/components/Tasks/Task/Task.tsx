c2907a5dfc96d5803c3159e589548c60e8f08929
tikaya <tatiana.chefox@gmail.com>
2023-10-25 19:04:28 +0300

    renaming attributes

--- src/components/Tasks/Task/Task.tsx
+++ src/components/Tasks/Task/Task.tsx
@@ -14,8 +14,8 @@ export const Task = ({id, id_projects, title, column}: TasksType) => {
         <div className={styles.task_container}>
             <p className={styles.title_task}>{title}</p>
             <svg onClick={() => removeTask(id)} className={styles.img_remove} viewBox="0 0 1024 1024" xmlns="http://www.w3.org/2000/svg" fill="#3d3d3d" stroke="#3d3d3d">
-                <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
-                <g id="SVGRepo_tracerCarrier" stroke-linecap="round" stroke-linejoin="round"></g>
+                <g id="SVGRepo_bgCarrier" strokeWidth="0"></g>
+                <g id="SVGRepo_tracerCarrier" strokeLinecap="round" strokeLinejoin="round"></g>
                 <g id="SVGRepo_iconCarrier">
                     <path d="M352 480h320a32 32 0 1 1 0 64H352a32 32 0 0 1 0-64z"></path>
                     <path d="M512 896a384 384 0 1 0 0-768 384 384 0 0 0 0 768zm0 64a448 448 0 1 1 0-896 448 448 0 0 1 0 896z"></path>
