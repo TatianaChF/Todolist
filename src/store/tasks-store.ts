31bf72263298d79b28193a0e1b40abbe5c22519d
tikaya <tatiana.chefox@gmail.com>
2023-10-31 12:16:00 +0300

    creating a store for tasks with actions

---
+++ src/store/tasks-store.ts
@@ -0,0 +1,25 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class TasksStore {
+    tasksList = [{
+        id: uuidv4(),
+        id_projects: 0,
+        title: "",
+        column: "Queue"
+    }];
+    
+    addTask = (title: string) => {
+        const task = {
+            id: uuidv4(),
+            id_projects: 0,
+            title: title,
+            column: "Queue"
+        }
+
+        this.tasksList.push(task);
+    }
+
+    removeTask = (id: string) => {
+        this.tasksList = this.tasksList.filter((task) => task.id !== id);
+    }
+}
