e2e3de8810a342f521a77ee6b4c7296787aef114
tikaya <tatiana.chefox@gmail.com>
2023-11-02 22:16:05 +0300

    wrap the project with the root store provider

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { DndProvider } from 'react-dnd';
 import './App.css';
 import { Outlet } from 'react-router';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { RootStoreContext } from './store/root-store-context';
+import { RootStore } from './store/roote-store';
 
 function App() {
   return (
-    <DndProvider backend={HTML5Backend}>
-      <div className="App">
-        <Outlet />
-      </div>
-    </DndProvider>
+    <RootStoreContext.Provider value={new RootStore()}>
+      <DndProvider backend={HTML5Backend}>
+        <div className="App">
+          <Outlet />
+        </div>
+      </DndProvider>
+    </RootStoreContext.Provider>
   );
 }
 
