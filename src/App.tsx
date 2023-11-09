b8cf8f4a54ac2fbd12e1bac07977515397d588ff
tikaya <tatiana.chefox@gmail.com>
2023-11-09 10:17:27 +0300

    delete store provider

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,15 @@ import './App.css';
 import { Outlet } from 'react-router';
 import { HTML5Backend } from 'react-dnd-html5-backend';
 import { RootStoreContext } from './store/root-store-context';
-import { RootStore } from './store/roote-store';
+import RootStore from './store/roote-store';
 
 function App() {
   return (
-    <RootStoreContext.Provider value={new RootStore()}>
       <DndProvider backend={HTML5Backend}>
         <div className="App">
           <Outlet />
         </div>
       </DndProvider>
-    </RootStoreContext.Provider>
   );
 }
 
