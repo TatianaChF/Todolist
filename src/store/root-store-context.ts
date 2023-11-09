c68c380c0aa96ca30938060008501f2be8d13e1b
tikaya <tatiana.chefox@gmail.com>
2023-11-09 09:46:17 +0300

    change store context

--- src/store/root-store-context.ts
+++ src/store/root-store-context.ts
@@ -1,10 +1,10 @@
 import { createContext, useContext } from "react";
-import { RootStore } from "./roote-store";
+import RootStore  from "./roote-store";
 
-export const RootStoreContext = createContext<RootStore | null>(null);
+export const RootStoreContext = createContext(RootStore);
 
 export const useStores = () => {
-    const context = useContext(RootStoreContext);
+    const context = useContext<typeof RootStore>(RootStoreContext);
 
     if (context === null) {
         throw new Error("Ошибка получения данных из стора");
