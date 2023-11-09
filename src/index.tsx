2e3f336d4bd4f14bd47f245362c3d30d3aa341af
tikaya <tatiana.chefox@gmail.com>
2023-11-09 10:17:00 +0300

    adding provider

--- src/index.tsx
+++ src/index.tsx
@@ -5,15 +5,19 @@ import reportWebVitals from './reportWebVitals';
 import { Providers } from './utils/Providers';
 import { RouterProvider } from 'react-router';
 import { router } from './utils/Router';
+import RootStore from './store/roote-store';
+import { Provider } from 'mobx-react';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Providers>
-      <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
-    </Providers>
+    <Provider value={RootStore}>
+      <Providers>
+        <RouterProvider router={router} fallbackElement={<div>Загрузка...</div>} />
+      </Providers>
+    </Provider>
   </React.StrictMode>
 );
 
