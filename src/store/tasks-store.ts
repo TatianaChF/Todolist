9c62fd048e74952cb2fcc8d0aa396f0c19cda194
tikaya <tatiana.chefox@gmail.com>
2023-11-11 10:43:57 +0300

    getting a list of tasks

--- src/store/tasks-store.ts
+++ src/store/tasks-store.ts
@@ -1,3 +1,27 @@
-class tasksStore {
-    
+import { makeAutoObservable } from "mobx";
+import { TasksType } from "../components/Tasks/Tasks";
+
+export class tasksStore {
+    tasks: TasksType[] = [];
+
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+    async fetchTasksData() {
+        try {
+            const res = fetch("./tasks.json").then(response => {
+                return response.json();
+            }).then(data => {
+                this.setTasks(data);
+                console.log(this.tasks);
+            })
+        } catch(error) {
+            console.log(error);
+        }
+    }
+
+    setTasks(tasksList: TasksType[]) {
+        this.tasks = tasksList;
+    }
 }
