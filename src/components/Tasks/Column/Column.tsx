5953fd8131c1918037a16a744c9210d04ca67293
tikaya <tatiana.chefox@gmail.com>
2023-11-23 12:26:37 +0300

    adding addItemToSection action

--- src/components/Tasks/Column/Column.tsx
+++ src/components/Tasks/Column/Column.tsx
@@ -1,23 +1,25 @@
 import { Dispatch } from "react";
-import { TasksType } from "../Tasks";
 import styles from "./Column.module.css";
 import { Task } from "../Task/Task";
 import { useDrop } from "react-dnd";
 import { observer } from "mobx-react-lite";
 import toast from "react-hot-toast";
+import { toJS } from "mobx";
+import { TasksType } from "../../../store/tasks-types";
 
 type ColumnPropsType = {
     status: string,
     items: TasksType[],
     setTasksAction: Dispatch<TasksType[]>,
     id: string | undefined,
-    removeTask: (id: string) => void
+    removeTask: (id: string) => void,
+    addItemToSection: (id: string, status: string) => void
 }
 
-export const Column = observer(({ status, items, setTasksAction, id, removeTask }: ColumnPropsType) => {
+export const Column = observer(({ status, items, setTasksAction, id, removeTask, addItemToSection }: ColumnPropsType) => {
     const [{ isOver }, drop] = useDrop(() => ({
         accept: "task",
-        drop: (item: TasksType) => addItemToSection(item.id),
+        drop: (item: TasksType) => addItemToSection(item.id, status),
         collect: (monitor) => ({
             isOver: !!monitor.isOver()
         })
@@ -31,8 +33,12 @@ export const Column = observer(({ status, items, setTasksAction, id, removeTask 
     //                 return {...task, column: status}
     //             }
 
+    //             console.log(task)
+
     //             return task;
     //         })
+
+    //         console.log(modifiedTask);
 
     //         return modifiedTask;
     //     });
@@ -49,7 +55,7 @@ export const Column = observer(({ status, items, setTasksAction, id, removeTask 
                     </h2>
                 </div>
                 {
-                    items?.filter(tasks => tasks.id_projects?.toString() === id).filter(task => task.column === status)
+                    (items as TasksType[]).filter(tasks => tasks.id_projects?.toString() === id).filter(task => task.column === status)
                         .map((value) => {
                             return (
                                 <Task key={value.id} id={value.id} id_projects={value.id_projects}
@@ -60,7 +66,4 @@ export const Column = observer(({ status, items, setTasksAction, id, removeTask 
             </div>
         )
     })
-function addItemToSection(id: string): any {
-    throw new Error("Function not implemented.");
-}
 
