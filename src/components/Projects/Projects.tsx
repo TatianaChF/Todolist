d435a8f6b250dc32db393aea8ee467f530d0c460
tikaya <tatiana.chefox@gmail.com>
2023-12-06 17:31:50 +0300

    adding observer

--- src/components/Projects/Projects.tsx
+++ src/components/Projects/Projects.tsx
@@ -4,6 +4,7 @@ import styles from "./Projects.module.css"
 import { Link } from "react-router-dom";
 import { useStore } from "../../utils/StoreProvider";
 import { CreateProject } from "../CreateProject/CreateProject";
+import { observer } from "mobx-react-lite";
 
 export type Project = {
     id: string,
@@ -11,28 +12,35 @@ export type Project = {
     status: string
 }
 
-export const Projects = () => {
+export const Projects = observer(() => {
     const store = useStore();
 
     useEffect(() => {
         store.projectsStore.fetchProjectsData();
-    }, [])
+    }, [store.projectsStore.fetchProjectsData])
 
     return (
         <>
             <h1 className={styles.header}>Projects</h1>
             <CreateProject addProjectsAction={store.projectsStore.addProjectsAction} />
+            {
+                store.projectsStore.projectsList?.map((value) => {
+                    return (
+                        value.title
+                    )
+                })
+            }
             <div>
-                {
-                    store.projectsStore.projectsList.map((value) => {
-                        return (
-                            <Link className={styles.link} to={`${value.id}`} key={value.id}>
-                                <Project key={value.id} id={value.id} title={value.title} status={value.status} />
-                            </Link>
-                        )
-                    })
-                }
+            {
+                store.projectsStore.projectsList?.map((value) => {
+                    return (
+                        <Link className={styles.link} to={`${value.id}`} key={value.id}>
+                            <Project key={value.id} id={value.id} title={value.title} status={value.status} />
+                        </Link>
+                    )
+                })
+            }
             </div>
         </>
     )
-}
+})
