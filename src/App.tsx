0b1282d7a6b5c96df97ede8af967a63881a4baf3
tikaya <tatiana.chefox@gmail.com>
2023-12-26 10:50:05 +0300

    adding a link to the registration page

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,18 @@ import { DndProvider } from 'react-dnd';
 import './App.css';
 import { Outlet } from 'react-router';
 import { HTML5Backend } from 'react-dnd-html5-backend';
+import { Link } from 'react-router-dom';
 
 function App() {
   return (
-      <DndProvider backend={HTML5Backend}>
-        <div className="App">
-          <Outlet />
-        </div>
-      </DndProvider>
+    <DndProvider backend={HTML5Backend}>
+      <div className="App">
+        <Link to="/registration">
+          <p>Регистрация</p>
+        </Link>
+        <Outlet />
+      </div>
+    </DndProvider>
   );
 }
 
