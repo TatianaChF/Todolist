85f92b014d7dfd645c11fb4fa12f6dc4c932b312
tikaya <tatiana.chefox@gmail.com>
2023-12-26 11:31:04 +0300

    adding divs for inputs

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,12 +1,18 @@
+import styles from "./Registration.module.css"
+
 export const Registration = () => {
     return (
         <div>
-            <form>
+            <form className={styles.form_registration}>
                 <h2>Registration</h2>
-                <label htmlFor="login"><b>Login</b></label>
-                <input type="text" placeholder="Enter login" name="login" id="login" />
-                <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter password" name="psw" id="psw" />
+                <div>
+                    <label htmlFor="login"><b>Login</b></label>
+                    <input type="text" placeholder="Enter login" name="login" id="login" />
+                </div>
+                <div>
+                    <label htmlFor="psw"><b>Password</b></label>
+                    <input type="password" placeholder="Enter password" name="psw" id="psw" />
+                </div>
                 <button type="submit">Register</button>
             </form>
         </div>
