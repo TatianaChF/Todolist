6b01aa3e41bfd0732a32822b19d4f7d133b3072a
tikaya <tatiana.chefox@gmail.com>
2023-12-26 20:46:20 +0300

    adding a button to go back

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -3,18 +3,21 @@ import styles from "./Registration.module.css"
 export const Registration = () => {
     return (
         <div>
-            <form className={styles.form_registration}>
-                <h1>Registration</h1>
-                <div>
-                    <label htmlFor="login"><b>Login</b></label>
-                    <input type="text" placeholder="Enter login" name="login" id="login" />
-                </div>
-                <div>
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter password" name="psw" id="psw" />
-                </div>
-                <button type="submit">Register</button>
-            </form>
+            <div className={styles.container}>
+                <form className={styles.form_registration}>
+                    <h1>Registration</h1>
+                    <div className={styles.form_input}>
+                        <label htmlFor="login"><b>Login</b></label>
+                        <input type="text" placeholder="Enter login" name="login" id="login" />
+                    </div>
+                    <div className={styles.form_input}>
+                        <label htmlFor="psw"><b>Password</b></label>
+                        <input type="password" placeholder="Enter password" name="psw" id="psw" />
+                    </div>
+                    <button type="submit">Register</button>
+                </form>
+                <button className={styles.btn_back}>Come back</button>
+            </div>
         </div>
     )
 }   
