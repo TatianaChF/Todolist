8aa56b428151e8207794060eccd7bb7ce0f64bdd
tikaya <tatiana.chefox@gmail.com>
2023-12-26 11:03:36 +0300

    adding redistration form

--- src/components/Registration/Registration.tsx
+++ src/components/Registration/Registration.tsx
@@ -1,7 +1,13 @@
 export const Registration = () => {
     return (
         <div>
-            
+            <form>
+                <h2>Registration</h2>
+                <label htmlFor="login"><b>Login</b></label>
+                <input type="text" placeholder="Enter login" name="login" id="login" />
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter password" name="psw" id="psw" />
+            </form>
         </div>
     )
 }
